180a237e6cdd2dd04e643151cb25cb54638effe8
aschultz25 <aschultz25@avenues.org>
2025-05-23 08:07:31 -0400

    Update sketch.js collision and ball multiploer

--- sketch.js
+++ sketch.js
@@ -9,10 +9,36 @@ function setup() {
 
 function draw() {
   background(220);
-  
+
+  let collisions = [];
   for (let i = 0; i < circles.length; i++) {
     for (let j = i + 1; j < circles.length; j++) {
-      circles[i].checkCollision(circles[j]);
+      if (circles[i].checkCollision(circles[j])) {
+        collisions.push([i, j]);
+      }
+    }
+  }
+
+  collisions.sort((a, b) => b[1] - a[1]);
+  for (let [i, j] of collisions) {
+    if (circles[i] && circles[j]) {  
+      
+      let centerX = (circles[i].position.x + circles[j].position.x) / 2;
+      let centerY = (circles[i].position.y + circles[j].position.y) / 2;
+
+      circles.splice(j, 1);
+      circles.splice(i, 1);
+
+      let numNew = 3
+      for (let k = 0; k < numNew; k++) {
+        circles.push(new Circle(
+          centerX,
+          centerY,
+          random(-3, 3),
+          random(-3, 3),
+          7 
+        ))
+      }
     }
   }
 
@@ -20,4 +46,3 @@ function draw() {
     c.update();
   }
 }
-
