a5ee98972deb2f8754c70760c76a7d785428e6a6
aschultz25 <aschultz25@avenues.org>
2025-05-23 08:13:04 -0400

    Update sketch.js deflected by mouse
    when my mouse goes near a ball it is supposed to change directions

--- sketch.js
+++ sketch.js
@@ -10,34 +10,16 @@ function setup() {
 function draw() {
   background(220);
 
-  let collisions = [];
-  for (let i = 0; i < circles.length; i++) {
-    for (let j = i + 1; j < circles.length; j++) {
-      if (circles[i].checkCollision(circles[j])) {
-        collisions.push([i, j]);
-      }
-    }
-  }
-
-  collisions.sort((a, b) => b[1] - a[1]);
-  for (let [i, j] of collisions) {
-    if (circles[i] && circles[j]) {  
-      
-      let centerX = (circles[i].position.x + circles[j].position.x) / 2;
-      let centerY = (circles[i].position.y + circles[j].position.y) / 2;
-
-      circles.splice(j, 1);
-      circles.splice(i, 1);
-
-      let numNew = 3
-      for (let k = 0; k < numNew; k++) {
-        circles.push(new Circle(
-          centerX,
-          centerY,
-          random(-3, 3),
-          random(-3, 3),
-          7 
-        ))
+  for (let i = circles.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (circles[i] && circles[j]) {
+        if (circles[i].checkCollision(circles[j])) {
+          let newCircles = circles[i].burst().concat(circles[j].burst());
+          circles.splice(i, 1);
+          circles.splice(j, 1);
+          circles = circles.concat(newCircles);
+          break;
+        }
       }
     }
   }
